302e71127335e05a2fe9abf4abe26836939d4c4e
rrsg123 <rrsg123@gmail.com>
2017-03-17 10:58:02 -0600

    corrections to 26

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
+
   def form_group_tag(errors, &block)
-    css_class = 'form-group'
-    css_class << ' has-error' if errors.any?
-    content_tag :div, capture(&block), class: css_class
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
   end
 end
