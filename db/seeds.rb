0d2728d05960a47969a1fe27ae3f34bf8bb492c5
rrsg123 <rrsg123@gmail.com>
2017-03-20 19:34:50 -0600

    finish 29

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,8 +33,9 @@ posts = Post.all
  
 100.times do
   Comment.create!(
+    user: users.sample,
     post: posts.sample,
-      body: RandomData.random_paragraph
+    body: RandomData.random_paragraph
   )
 end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Post, type: :model do
-  let(:title) { RandomData.random_sentence }
-  let(:body) { RandomData.random_paragraph }
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: title, body: body, user: user) }  
 
+  it { is_expected.to have_many(:comments) }
 
   it { should belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
