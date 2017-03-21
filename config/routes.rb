80c4f67a952f81f6846a54aaa457c8481f8e1cf4
rrsg123 <rrsg123@gmail.com>
2017-03-21 10:47:37 -0600

    finish 30

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 Rails.application.routes.draw do
   
   resources :topics do
-
     resources :posts, except: [:index]
-   
   end
 
-  resources :posts, only: [] do
-   
+   resources :posts, only: [] do
      resources :comments, only: [:create, :destroy]
+
+     post '/up-vote' => 'votes#up_vote', as: :up_vote
+     post '/down-vote' => 'votes#down_vote', as: :down_vote
+
   
   end
 
-
   resources :users, only: [:new, :create]
-
   resources :sessions, only: [:new, :create, :destroy]
   
   get 'about' => 'welcome#about'
-
   root 'welcome#index' 
 
 end
